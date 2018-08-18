23f59fa350793d8968cafc179114c15152157986
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2018-08-18 16:24:36 +0200

    update subscriptions

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,36 @@
 'use strict';
 import * as vscode from 'vscode';
 
+interface UsesFormatterState {
+    context: vscode.ExtensionContext;
+}
+
+function formatUses(doc: vscode.TextDocument) {
+    vscode.window.showInformationMessage('Not implemented yet!');
+}
+
+function formatUsesOnCommand() {
+    if (!vscode.window.activeTextEditor) {
+        vscode.window.showErrorMessage('No active text editor found!');
+        return;
+    }
+    formatUses(vscode.window.activeTextEditor.document);
+}
+
+function subscribe(state: UsesFormatterState) {
+    vscode.workspace.onDidSaveTextDocument(formatUses, state, state.context.subscriptions);
+
+    let commandDisposable = vscode.commands.registerCommand('extension.formatUses', formatUsesOnCommand, state);
+    state.context.subscriptions.push(commandDisposable);
+}
+
 export function activate(context: vscode.ExtensionContext) {
     console.log('Format uses extension is activated');
 
-    let disposable = vscode.commands.registerCommand('extension.formatUses', () => {
-        vscode.window.showInformationMessage('Not implemented yet!');
-    });
-
-    context.subscriptions.push(disposable);
+    let state: UsesFormatterState = {context};
+    subscribe(state);
 }
 
 export function deactivate() {
+    console.log('Format uses extension is deactivated');
 }
