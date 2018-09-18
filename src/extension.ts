5aa7a11bc080dc359390f5f1a0af3a94c11e4fd3
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2018-09-18 21:10:03 +0200

    add basic parsing of the file and non working creation of new uses section

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,48 @@
 'use strict';
 import * as vscode from 'vscode';
 
-interface UsesFormatterState {
+interface IUsesFormatterState {
     context: vscode.ExtensionContext;
+}
+
+interface IUsesSectionData {
+    fullText: string;
+    index: number;
+    units: string[];
+    separator: string;
+}
+
+function parseUsingSections(text: string): IUsesSectionData[]
+{
+    const usesSectionRegex = /uses(\s)((?:[^;])+);/g;
+    let results: IUsesSectionData[] = [];
+    let match = null;
+    while ((match = usesSectionRegex.exec(text)) !== null) {
+        results.push({
+            index: match.index,
+            fullText: match[0],
+            separator: match[1],
+            units: match[2].replace(/(\r\n\t|\n|\r\t|\s)/gm,"").split(',')
+        });
+    }
+
+    return results;
+}
+
+function formatUsesSection(usesSection: IUsesSectionData): string
+{
+    let section = `uses ${usesSection.separator}`;
+    return section;
 }
 
 function formatUses(doc: vscode.TextDocument) {
     vscode.window.showInformationMessage('Not implemented yet!');
-    let text = doc.getText();
-    let regex = /uses((.|\s)*?);/g;
-    let matches = text.match(regex);
-    console.log(matches);
+    const text = doc.getText();
+    const usesSections = parseUsingSections(text);
+    usesSections.forEach(section => section.units.sort());
+    const newSections = usesSections.map(formatUsesSection);
+    console.log(usesSections);
+    console.log(newSections);
 }
 
 function formatUsesOnCommand() {
@@ -21,7 +53,7 @@ function formatUsesOnCommand() {
     formatUses(vscode.window.activeTextEditor.document);
 }
 
-function subscribe(state: UsesFormatterState) {
+function subscribe(state: IUsesFormatterState) {
     vscode.workspace.onDidSaveTextDocument(formatUses, state, state.context.subscriptions);
 
     let commandDisposable = vscode.commands.registerCommand('extension.formatUses', formatUsesOnCommand, state);
@@ -31,7 +63,7 @@ function subscribe(state: UsesFormatterState) {
 export function activate(context: vscode.ExtensionContext) {
     console.log('Format uses extension is activated');
 
-    let state: UsesFormatterState = {context};
+    let state: IUsesFormatterState = {context};
     subscribe(state);
 }
 
