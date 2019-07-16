32fe96126229b281b0666e01dfd574f45695cf11
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2019-07-16 20:57:02 +0200

    add generationg of edit object

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,22 @@
 import * as vscode from 'vscode';
-import { formatText } from './usesFormatter';
-import { TextEditor, TextEditorEdit } from 'vscode';
+import { formatText, ITextSection } from './usesFormatter';
+import { TextEditor, TextEditorEdit, Range } from 'vscode';
 
 interface IUsesFormatterState {
   context: vscode.ExtensionContext;
 }
 
-function formatDocument(doc: vscode.TextDocument, _edit: TextEditorEdit)
+function formatDocument(doc: vscode.TextDocument, edit: TextEditorEdit)
 {
   const separator = "  ";
   const lineEnd = "\n";
   const text = doc.getText();
   vscode.window.showInformationMessage('Not implemented yet!');
-  formatText(text, separator, lineEnd);
+  const newSections = formatText(text, separator, lineEnd);
+  newSections.forEach((section: ITextSection) => {
+    const range = new Range(doc.positionAt(section.startOffset), doc.positionAt(section.endOffset));
+    edit.replace(range, section.value);
+  });
 }
 
 function formatUsesOnCommand(textEditor: TextEditor, edit: TextEditorEdit) {
