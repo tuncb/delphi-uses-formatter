5627ade6e9d87f6450c6702a1a2f72ea01875b98
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2024-11-23 18:20:14 +0100

    add more specific tests for matching only whole 'uses' keyword

--- src/test/suite/usesFormatter.test.ts
+++ src/test/suite/usesFormatter.test.ts
@@ -1,7 +1,7 @@
 import { formatText, ITextSection } from '../../usesFormatter';
 
 var expect = require('chai').expect;
-var {describe, it} = require('mocha');
+var { describe, it } = require('mocha');
 
 interface ITestInput {
   text: string;
@@ -22,7 +22,7 @@ const sampleTexts: TestSample[] = [
     output: [
       {
         startOffset: 0,
-        endOffset:22,
+        endOffset: 22,
         value: "uses\n  a,\n  b,\n  c,\n  d,\n  e,\n  f;"
       }
     ],
@@ -35,16 +35,35 @@ const sampleTexts: TestSample[] = [
     output: [
       {
         startOffset: 0,
-        endOffset:22,
+        endOffset: 22,
         value: "uses\n  c,\n  f,\n  a,\n  b,\n  d,\n  e;"
       }
     ],
   },
+  {
+    input: {
+      text: "}uses d, c, b, e, f, a;",
+      configurableSortingArray: ["c", "f"]
+    },
+    output: [
+      {
+        startOffset: 1,
+        endOffset: 23,
+        value: "uses\n  c,\n  f,\n  a,\n  b,\n  d,\n  e;"
+      }
+    ],
+  },
+  {
+    input: {
+      text: "kuses d, c, b, e, f, a;",
+      configurableSortingArray: ["c", "f"]
+    },
+    output: [],
+  },
 
 ];
 
-const test = (sample: TestSample): void =>
-{
+const test = (sample: TestSample): void => {
   const separator = "  ";
   const lineEnd = "\n";
 
@@ -52,9 +71,9 @@ const test = (sample: TestSample): void =>
   expect(replaces).to.eql(sample.output);
 };
 
-describe('UsesFormatter', function() {
+describe('UsesFormatter', function () {
   describe('formatText', function () {
-    it('Expect correct  replaceText for provided  samples',  function() {
+    it('Expect correct  replaceText for provided  samples', function () {
       sampleTexts.forEach(test);
     });
   });
